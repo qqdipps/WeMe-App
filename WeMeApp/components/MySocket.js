86d8a0fe5afb7ff1125d10cebdc556fc8f2462eb
qqdipps <sdipp007@gmail.com>
2019-07-12 03:30:38 -0700

    created component MySocket, connecting to backend via websocket

--- WeMeApp/components/MySocket.js
+++ WeMeApp/components/MySocket.js
@@ -7,9 +7,33 @@ class MySocket extends Component {
     super();
     this.state = { socket: undefined };
   }
+  componentDidMount = () => {
+    if (!this.state.socket) {
+      this.initializeSocket();
+    }
+  };
 
   initializeSocket = () => {
-    const mySocket = new Socket();
+    const mySocket = new Socket("ws://192.168.1.12:4000/socket");
+
+    mySocket.connect();
+
+    mySocket.onOpen(() => {
+      console.log("Socket connection to websocket on WeMeAPI");
+    });
+
+    mySocket.onClose(e => {
+      // connection closed
+      console.log(e.code, e.reason);
+    });
+
+    mySocket.onError(e => {
+      // an error occurred
+      console.log("error");
+      console.log(e.message);
+    });
+
+    this.setState({ socket: mySocket });
   };
 
   render() {
