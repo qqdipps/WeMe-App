99910d94fdfc30563ccbfa923f6688178287ae5d
qqdipps <sdipp007@gmail.com>
2019-07-14 04:17:51 -0700

    updated schema

--- WeMeApp/App.js
+++ WeMeApp/App.js
@@ -16,14 +16,16 @@ class App extends Component {
           name: "UserSelf",
           properties: {
             userId: "int",
-            displayName: "string"
+            displayName: "string",
+            channels: "string?[]"
           }
         },
         {
           name: "ConnectionEncryption",
           properties: {
             connectionId: "int",
-            encryptionKey: "string"
+            encryptionKey: "string",
+            inUse: "bool"
           }
         },
         {
@@ -39,7 +41,6 @@ class App extends Component {
         }
       ]
     };
-    console.log(this.state.schema.UserSelfSchema);
   }
 
   setSocket = socket => {
