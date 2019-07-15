718c0ff1f8ff780d1f2f9e8bd6d5a0c601204fc7
qqdipps <sdipp007@gmail.com>
2019-07-15 13:15:09 -0700

    generate password and salt randomly

--- WeMeApp/functions/AESfunctions.js
+++ WeMeApp/functions/AESfunctions.js
@@ -3,16 +3,21 @@ import { NativeModules } from "react-native";
 // return promise, key
 export function generateKey() {
   const Aes = NativeModules.Aes;
-  const generateKey = (password, salt, cost, length) => {
-    return Aes.pbkdf2(password, salt, cost, length);
-  };
+  const password = Math.random()
+    .toString(32)
+    .substring(2);
+  const salt = Math.random()
+    .toString(32)
+    .substring(2);
+  const cost = 5000;
+  const length = 128; // 128 || 192 || 256, 256 increase ~40% runtime for generation.
+  return Aes.pbkdf2(password, salt, cost, length);
 }
 
 // return promise, encryptedData ({ cipher, iv })
 export function encryptMessage(message, keyBase64) {
   const Aes = NativeModules.Aes;
   return Aes.randomKey(16).then(iv => {
-    console.log("IV: ", iv);
     return Aes.encrypt(message, keyBase64, iv).then(cipher => ({
       cipher,
       iv
