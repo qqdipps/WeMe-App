34bfe70673cc6a36e4093c2df9c817ccb511c3b5
qqdipps <sdipp007@gmail.com>
2019-07-14 23:21:24 -0700

    success linking AES Java library, updated minSdkVersion from 16 to 10.

--- WeMeApp/android/app/src/main/java/com/wemeapp/MainApplication.java
+++ WeMeApp/android/app/src/main/java/com/wemeapp/MainApplication.java
@@ -6,6 +6,7 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.tectiv3.aes.RCTAesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -24,8 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       @SuppressWarnings("UnnecessaryLocalVariable")
       List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // Packages that cannot be autolinked yet can be added manually here, for
+      // example:
       // packages.add(new MyReactNativePackage());
+      packages.add(new RCTAesPackage());
       return packages;
     }
 
