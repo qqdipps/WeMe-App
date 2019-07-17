4a8cf2b6b5fcc8dae33a62c157f717831987719f
qqdipps <sdipp007@gmail.com>
2019-07-17 08:43:46 -0700

    wr in process

---
+++ WeMeApp/components/QrGenerator.js
@@ -0,0 +1,27 @@
+// import React, { Component } from "react";
+// import QRCode from "react-native-qrcode";
+// import { View } from "react-native";
+
+// class QrGenerator extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       valueForQRCode: "TEST"
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.MainContainer}>
+//         <QRCode
+//           value={this.state.valueForQRCode}
+//           size={250}
+//           bgColor="#000"
+//           fgColor="#fff"
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+// export default QrGenerator;
---
+++ WeMeApp/screens/SpawnScreen.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import { View } from "react-native";
+import QrGenerator from "../components/QrGenerator";
+
+class SpawnScreen extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    <QrGenerator />;
+  }
+}
+
+export default SpawnScreen;
